2c94c98fc53970f0d4a121282b25efbc9b3267ab
ansab210 <rajpootansab2@gmail.com>
2025-02-19 12:42:10 -0800

    Create Footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,48 @@
+import { assets } from "@/assets/assets";
+import React from "react";
+
+function footer() {
+  return (
+    <>
+      <div className="flex flex-col sm:flex-row justify-between  gap-4 sm:my-10 sm:mt-40 my-5 mt-20">
+        <div className="w-full sm:w-[60%]">
+          <img className="sm:w-[30%] w-36" src={assets.logo} alt="LOGO" />
+          <p className="text-gray-500 pt-2 leading-5 text-xs sm:text-sm md:text-sm lg:text-xl sm:leading-6 w-full md:w-3/4">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+        <div className="w-full sm:w-[20%]">
+          <p className="text-gray-600 text-sm sm:text-sm md:text-sm lg:text-xl font-semibold">
+            COMPANY
+          </p>
+          <ul className="sm:mt-7 mt-4 flex flex-col sm:gap-2  gap-1 text-sm sm:text-sm md:text-sm lg:text-xl text-gray-600 cursor-pointer">
+            <li className="hover:text-black">Home</li>
+            <li className="hover:text-black">About Us</li>
+            <li className="hover:text-black">Delivery</li>
+            <li className="hover:text-black">Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="w-full sm:w-[20%]">
+          <p className="text-gray-600 text-sm sm:text-sm md:text-sm lg:text-xl font-semibold">
+            GET IN TOUCH
+          </p>
+          <ul className="sm:mt-7 mt-4 flex flex-col sm:gap-2  gap-1 text-sm sm:text-sm md:text-sm lg:text-xl text-gray-600 cursor-pointer ">
+            <li className="hover:text-black">9-82387327-1</li>
+            <li className="hover:text-black">info@trisagesolution.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className="text-center py-2 md:py-5 sm:text-sm md:text-sm lg:text-sm text-[10px] text-gray-600">
+          Copyright 2025 © TrisageSolutions.com - All Right Reserved.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default footer;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import NavBar from "@/components/navbar/navBar";
+import Footer from "@/components/footer/footer";
 import ShopContextProvider from "@/components/context/shopContext";
 
 export default function App({ Component, pageProps }) {
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
         <NavBar />
         <Component {...pageProps} />
+        <Footer />
       </div>
     </ShopContextProvider>
   );
